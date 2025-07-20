6240e5c57af56a340359bd6b07b5087c11c3e1bd
Sunner <sunnerlp@gmail.com>
2025-07-20 23:54:49 +0200

    Enable experimental features

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -22,6 +22,11 @@
 		};
 	};
 
+	nix.settings.experimental-features = [
+		"nix-command"
+		"flakes"
+	];
+
 	# Pick only one of the below networking options.
 	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 	# networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
