9ef0ef80fdad34776ff825dc9dcf35eb05795704
Sunner <sunnerlp@gmail.com>
2025-08-05 13:04:56 +0200

    Update window rules

--- home/users/robin/hyprland/rules.nix
+++ home/users/robin/hyprland/rules.nix
@@ -7,13 +7,21 @@ in
 			# Give kitty some transparency
 			"opacity 0.75, class:kitty"
 
-			# Launch Firefox PiP in floating mode
-			"float, class:firefox, title:Picture-in-Picture"
+			# Launch Firefox PiP in floating mode and pin it
+			"tag +pip, class:firefox, title:Picture-in-Picture"
+			"tag +pip, class:firefox, title:Bild-im-Bild"
+			
+			"float, tag:pip"
+			"pin, tag:pip"
+			"content video, tag:pip"
+			"move 100%-w-8 100%-w-${toString (waybarHeight + 8)}, tag:pip"
+
 			
 			# Move all windows launched by tray/waybar modules
 			# to the bottom right corner
 			"tag +tray, class:org.pulseaudio.pavucontrol"
 			"float, tag:tray"
+			"pin, tag:tray"
 			"move 100%-w-8 100%-w-${toString (waybarHeight + 8)}, tag:tray"
 
 			"bordersize 0, floating:0, onworkspace:w[tv1]"
