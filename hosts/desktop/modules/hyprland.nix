26a4c377c62b7cd093d4a6ccd6d18b2fd849ecd4
Sunner <sunnerlp@gmail.com>
2025-08-18 14:20:11 +0200

    Use hyprland for regreet to fix multi monitor setup

--- hosts/desktop/modules/hyprland.nix
+++ hosts/desktop/modules/hyprland.nix
@@ -1,26 +1,55 @@
-{ pkgs, ... }: {
-	programs = {
-		hyprland = {
-			enable = true;
-			withUWSM = true;
-		};
+{ lib, pkgs, ... }: let
+  inherit (lib) getExe getExe';
 
-		regreet = {
-			enable = true;
-		};
-	};
+  greetdHyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
+    animations {
+      enabled=false
+      first_launch_animation=false
+    }
 
-	services = {
-		# For udiskie (auto-mounting drives)
-		udisks2.enable = true;
-		# For gammastep (red filter at night)
-		geoclue2.enable = true;
-	};
+    input {
+      kb_layout=de
+    }
 
-	environment.systemPackages = with pkgs; [
-		kitty
+    monitor = ,highres,auto,1
 
-		waybar
-		wev
-	];
+    exec-once = ${lib.getExe' pkgs.dbus "dbus-update-activation-environment"} --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP HYPRLAND_INSTANCE_SIGNATURE
+    exec-once = ${getExe pkgs.greetd.regreet}; ${getExe' pkgs.hyprland "hyprctl"} dispatch exit
+  '';
+in {
+  programs = {
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+
+    regreet = {
+      enable = true;
+    };
+  };
+
+  services = {
+    # For udiskie (auto-mounting drives)
+    udisks2.enable = true;
+    # For gammastep (red filter at night)
+    geoclue2.enable = true;
+
+
+    greetd = {
+      settings = {
+        default_session = {
+          command = "${getExe pkgs.hyprland} --config ${greetdHyprlandConfig} > /tmp/hyprland-log-out.txt 2>&1";
+        };
+      };
+
+      restart = false;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+
+    waybar
+    wev
+  ];
 }
