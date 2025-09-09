25dae24b2f2858848caa6edbc1008ca258dc1e88
Sunner <sunnerlp@gmail.com>
2025-09-09 22:14:40 +0200

    Update system (again)

--- hosts/desktop/pc/hardware-configuration.nix
+++ hosts/desktop/pc/hardware-configuration.nix
@@ -1,39 +1,44 @@
-{ inputs, ... }: {
-	imports = [
-		inputs.nixos-hardware.nixosModules.common-cpu-amd
-		inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
-		inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
-		inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
-		inputs.nixos-hardware.nixosModules.common-pc
-		inputs.nixos-hardware.nixosModules.common-pc-ssd
-	];
+{ inputs, lib, pkgs, ... }: let
+  inherit (lib) mkForce;
+in {
+  imports = [
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
+    inputs.nixos-hardware.nixosModules.common-cpu-amd-zenpower
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+    inputs.nixos-hardware.nixosModules.common-pc
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
+  ];
 
-	boot = {
-		kernelModules = [ "kvm-amd" ];
-		kernelParams = [ "module_blacklist=amdgpu" ];
-		extraModulePackages = [ ];
+  boot = {
+    kernelModules = [ "kvm-amd" ];
+    kernelParams = [ "module_blacklist=amdgpu" ];
+    extraModulePackages = [ ];
 
-		initrd = {
-			availableKernelModules = [
-				"xhci_pci"
-				"nvme"
-				"ahci"
-				"thunderbolt"
-				"usb_storage"
-				"usbhid"
-				"sd_mod"
-				"sr_mod"
-			];
+    # Force LTS kernel for NVIDIA build to succeed: https://github.com/NixOS/nixpkgs/issues/429624
+    kernelPackages = mkForce pkgs.linuxPackages_6_12;
 
-			kernelModules = [ ];
-		};
-	};
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "nvme"
+        "ahci"
+        "thunderbolt"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "sr_mod"
+      ];
 
-	nixpkgs.hostPlatform = "x86_64-linux";
+      kernelModules = [ ];
+    };
+  };
 
-	hardware = {
-		enableRedistributableFirmware = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
 
-		nvidia.open = true;
-	};
+  hardware = {
+    enableRedistributableFirmware = true;
+
+    nvidia.open = true;
+  };
 }
