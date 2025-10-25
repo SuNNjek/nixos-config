dbba1225a16bf82fbd72cd88b9c5c3a259beb244
Sunner <sunnerlp@gmail.com>
2025-10-25 23:56:20 +0200

    Remove Steam flatpak autostart and add Minecraft launcher

--- home/use-cases/gaming.nix
+++ home/use-cases/gaming.nix
@@ -1,20 +1,30 @@
 { lib, osConfig, pkgs, ... }: let 
   cfg = osConfig.sunner.useCases.gaming;
-in lib.mkIf cfg.enable {
-  programs.mangohud.enable = true;
-
-  xdg = {
-    autostart = {
-      entries = with pkgs; [
-        "${steam}/share/applications/steam.desktop"
+in lib.mkIf cfg.enable (
+  lib.mkMerge [
+    {
+      home.packages = with pkgs; [
+        prismlauncher
       ];
 
-      flatpaks = {
+      programs.mangohud.enable = true;
+
+      xdg = {
+        autostart = {
+          entries = with pkgs; [
+            "${steam}/share/applications/steam.desktop"
+          ];
+        };
+      };
+    }
+
+    (lib.mkIf osConfig.sunner.flatpak.enable {
+      xdg.autostart.flatpaks = {
         "com.valvesoftware.Steam" = {
           enable = true;
           args = [ "-silent" ];
         };
       };
-    };
-  };
-}
+    })
+  ]
+)
