6cebeee58eea41f63f9bd3ed2cce951ab31d806b
Sunner <sunnerlp@gmail.com>
2025-11-04 22:04:45 +0100

    Update DMS again

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
     };
 
     dankMaterialShell = {
-      url = "github:AvengeMedia/DankMaterialShell/v0.3.2";
+      url = "github:AvengeMedia/DankMaterialShell/v0.3.4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
