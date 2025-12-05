0cbf8cb00d2f9369f86acdc5e3f2e1e44c0702e3
Sunner <sunnerlp@gmail.com>
2025-12-05 16:40:06 +0100

    chore: updates

--- hosts/modules/configs/limine.nix
+++ hosts/modules/configs/limine.nix
@@ -7,38 +7,6 @@ in lib.mkMerge [
 
       # Should be enough and doesn't spam the boot menu
       maxGenerations = 5;
-
-      # No stylix support yet, so do it manually
-      style = lib.mkIf cfg.enableStylix (let
-        inherit (config.lib.stylix) colors;
-      in {
-        wallpapers = [ config.stylix.image ];
-        backdrop = colors.base00;
-
-        graphicalTerminal = {
-          palette = builtins.concatStringsSep "," (with colors; [
-            "000000"
-            red
-            green
-            brown
-            blue
-            magenta
-            cyan
-            "808080"
-          ]);
-
-          brightPalette = builtins.concatStringsSep "," (with colors; [
-            "808080"
-            bright-red
-            bright-green
-            brown
-            bright-blue
-            bright-magenta
-            bright-cyan
-            "FFFFFF"
-          ]);
-        };
-      });
     };
   }
 
